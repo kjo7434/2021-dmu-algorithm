6a1b4cc54380b74bc765225e9a872781771a4b2b
kjo7434 <48851316+kjo7434@users.noreply.github.com>
2021-09-25 13:42:26 +0900

    Add files via upload

---
+++ Week3_qus01.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Week3_qus01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int result = 0;
+        while (true) {
+
+        int target = (n / k) * k;
+        result += (n - target);
+        n = target;
+
+        if (n < k) break;
+            result += 1;
+            n /= k;
+        }
+
+        result += (n - 1);
+        System.out.println(result);
+    }
+}
+
