9bb58e002df002d84cfe360cc1b5673be61edd9a
kjo7434 <kjo7434@naver.com>
2021-11-10 21:07:30 +0900

    10주차 과제

---
+++ 10주차/Week10_qus01.py
@@ -0,0 +1,6 @@
+N=int(input("Input: "))
+if (N^1)==(N+1) :
+    res="Even"
+else :
+    res="Odd"
+print("Output :",res)
---
+++ 10주차/Week10_qus03.py
@@ -0,0 +1,20 @@
+import math
+ 
+def getPosOfRightmostSetBit(n):
+    return int(math.log2(n&-n)+1)
+ 
+  
+def setRightmostUnsetBit(n):
+    if (n == 0):
+        return 1
+      
+    if ((n & (n + 1)) == 0):   
+        return n
+      
+    pos = getPosOfRightmostSetBit(~n)   
+      
+    return ((1 << (pos - 1)) | n)
+ 
+n = int(input("Input:"))
+print("Output:",end="")
+print(setRightmostUnsetBit(n))
