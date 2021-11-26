a97ba6c9e9a119bdc6fbcef11889eb67142ad7cc
kjo7434 <kjo7434@naver.com>
2021-11-26 19:24:25 +0900

    12주차

---
+++ 12주차/Week12_qus01.py
@@ -0,0 +1,10 @@
+from itertools import permutations
+def largest(l):
+    lst = []
+    for i in permutations(l, len(l)):
+        # provides all permutations of the list values,
+        # store them in list to find max
+        lst.append("".join(map(str,i)))
+    return max(lst)
+ 
+print(largest([54, 546, 548, 60]))
